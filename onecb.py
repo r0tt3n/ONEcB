911c92deb37b2116e230887d892326f6b7d42380
r0tt3n <r0tt3npunk@gmail.com>
2015-02-05 08:35:59 -0500

    removed test string

--- onecb.py
+++ onecb.py
@@ -1,5 +1,4 @@
 ### ONEcB - One Click Backup v1.0 by G.C. Gerba
-import ttk
 import os
 import shutil
 from tkinter import *
@@ -8,25 +7,28 @@ gui = Tk()
 gui.geometry('200x100')
 gui.title("ONEcB")
 path = '~'
-destination = str(os.getcwd()) +  str("/backup")
+destination = str(os.getcwd()) + str("/backup")
 source = os.path.expanduser(path)
 photo = PhotoImage(file="onecb.gif")
 label = Label(image=photo).place(x=35, y=5)
+
+
 def copyhome():
     if os.name == 'posix' or os.name == 'nt':
         try:
-            shutil.copytree(source +str("/test"), destination)
+            shutil.copytree(source, destination)
             tkinter.messagebox.showinfo("Success!", "Files were successfully copied")
         except:
             tkinter.messagebox.showerror("Error", "Permission Denied\nor folder alredy exists")
     else:
         print("Unrecognized OS")
 copy = Button(text="Copy to USB", command=copyhome)
-copy.place(x=20,y=60)
+copy.place(x=20, y=60)
 guiexit = Button(text="Exit", command=gui.quit)
-guiexit.place(x=130,y=60)
+guiexit.place(x=130, y=60)
 gui.mainloop()
 
 ### TODO:
 ### progress bar.
+### specify file type
 ### real world test.
